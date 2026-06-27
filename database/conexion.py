3c888016ba54d67cc36e57a6c2036c4db444c92c
GaelFelix29 <gabriel.felix@vital-healtl-global.com>
2026-06-27 00:57:33 -0600

    migracion a aiven 3

--- database/conexion.py
+++ database/conexion.py
@@ -7,8 +7,16 @@ HOST = os.getenv("MYSQLHOST")
 PUERTO = os.getenv("MYSQLPORT")
 BASE_DATOS = os.getenv("MYSQLDATABASE")
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SSL_CERT = os.path.join(BASE_DIR, "ca.pem")
+
 engine = create_engine(
     f"mysql+pymysql://{USUARIO}:{PASSWORD}@{HOST}:{PUERTO}/{BASE_DATOS}",
     pool_pre_ping=True,
-    pool_recycle=3600
+    pool_recycle=3600,
+    connect_args={
+        "ssl": {
+            "ca": SSL_CERT
+        }
+    }
 )
