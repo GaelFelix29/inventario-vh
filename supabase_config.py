b7d2e94961442ac7f1e6cc2f109292e71dd3c7b7
GaelFelix29 <gabriel.felix@vital-healtl-global.com>
2026-07-09 10:46:00 -0600

    0124

--- supabase_config.py
+++ supabase_config.py
@@ -1,18 +1,13 @@
 import os
-
-from supabase_config import supabase
-
 from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+print("URL:", repr(SUPABASE_URL))
+print("KEY:", repr(SUPABASE_KEY))
+
 supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
 )
-
-print("================================")
-print("SUPABASE CONECTADO")
-print("URL:", SUPABASE_URL)
-print("================================")
