6cfaca11e8c80d8699230708d028528d8ec63b20
GaelFelix29 <gabriel.felix@vital-healtl-global.com>
2026-07-09 13:05:40 -0600

    PRUEBAA 201

--- supabase_config.py
+++ supabase_config.py
@@ -1,14 +1,8 @@
-import os
 from supabase import create_client
+import os
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
-print("=" * 50)
-print("URL:", repr(SUPABASE_URL))
-print("KEY:", repr(SUPABASE_KEY))
-print("LONGITUD:", len(SUPABASE_KEY) if SUPABASE_KEY else 0)
-print("=" * 50)
 
 supabase = create_client(
     SUPABASE_URL,
