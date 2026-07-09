be3804662398ab61dc6355b743757e4e3b05f5d9
GaelFelix29 <gabriel.felix@vital-healtl-global.com>
2026-07-09 10:43:47 -0600

    clooudinary prueba 4

--- supabase_config.py
+++ supabase_config.py
@@ -1,5 +1,7 @@
 import os
 
+from supabase_config import supabase
+
 from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
@@ -9,3 +11,8 @@ supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
 )
+
+print("================================")
+print("SUPABASE CONECTADO")
+print("URL:", SUPABASE_URL)
+print("================================")
